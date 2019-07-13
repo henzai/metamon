c205ea8174f22e73afc5d7985ea2df05e63422c7
ryosuke watanabe <ry0chord@gmail.com>
2019-07-14 00:09:41 +0900

    firebase authをセッティング

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
+import firebase from "./auth/firebase";
 import "./App.css";
 
+async function login() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  await firebase.auth().signInWithPopup(provider);
+}
+
+async function logout() {
+  await firebase.auth().signOut();
+}
+
 const App: React.FC = () => {
+  const [user, setUser] = useState<firebase.User | null>(
+    firebase.auth().currentUser
+  );
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+      setUser(user);
+    });
+    return () => unsubscribe();
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="App-intro">UID: {user && user.uid}</p>
+
+      {user ? (
+        <button onClick={e => logout()}>Google Logout</button>
+      ) : (
+        <button onClick={e => login()}>Google Login</button>
+      )}
     </div>
   );
 };
